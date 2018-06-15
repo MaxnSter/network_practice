e1ab2a6286090981a198034fd9a3aee41205c309
maxnster <maxnster20@gmail.com>
2018-06-16 00:35:01 +0800

    增加并行度

--- median/median_grpc/client/median_client.go
+++ median/median_grpc/client/median_client.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"math"
 	"os"
+	"sync"
 
 	"github.com/MaxnSter/network_practice/median/kth"
 	pb "github.com/MaxnSter/network_practice/median/median_grpc"
@@ -13,19 +14,29 @@ import (
 
 type collector struct {
 	connections []pb.MedianClient
+	wg          sync.WaitGroup
 
 	min, max   int64
 	count, sum int64
 }
 
 func (c *collector) Connect(addrs ...string) {
+	connCh := make(chan *grpc.ClientConn, len(addrs))
 	for _, addr := range addrs {
-		conn, err := grpc.Dial(addr, grpc.WithInsecure())
-		if err != nil {
-			panic(err)
-		}
+		go func(addr string) {
+			conn, err := grpc.Dial(addr, grpc.WithInsecure())
+			if err != nil {
+				panic(err)
+			}
+			connCh <- conn
+		}(addr)
+	}
+
+	for i := 0; i < len(addrs); i++ {
+		conn := <-connCh
 		c.connections = append(c.connections, pb.NewMedianClient(conn))
 	}
+	close(connCh)
 }
 
 func (c *collector) reset() {
@@ -38,12 +49,24 @@ func (c *collector) reset() {
 func (c *collector) Generate(min, max, count int64) {
 	c.reset()
 	req := &pb.GenerateRequest{Count: count, Max: max, Min: min}
-	for _, conn := range c.connections {
-		response, err := conn.Generate(context.Background(), req)
-		if err != nil {
-			panic(err)
-		}
+	resCh := make(chan *pb.QueryResponse, len(c.connections))
 
+	for _, conn := range c.connections {
+
+		conn := conn
+		go func() {
+			response, err := conn.Generate(context.Background(), req)
+			if err != nil {
+				panic(err)
+			}
+
+			resCh <- response
+		}()
+
+	}
+
+	for i := 0; i < len(c.connections); i++ {
+		response := <-resCh
 		c.sum += response.Sum
 		c.count += response.Count
 		if c.min > response.Min {
@@ -53,25 +76,39 @@ func (c *collector) Generate(min, max, count int64) {
 			c.max = response.Max
 		}
 	}
+	close(resCh)
 
 	fmt.Printf("min:%d, max:%d, count:%d, sum:%d, avg:%d\n",
 		c.min, c.max, c.count, c.sum, c.sum/c.count)
 }
 
-func (c *collector) FindKthMax(k int) (int, bool){
+func (c *collector) FindKthMax(k int) (int, bool) {
 	return kth.FindKth(c.search, k, int(c.count), int(c.min), int(c.max))
 }
 
 func (c *collector) search(guest int) (smaller, same int) {
 	req := &pb.SeachRequest{Guess: int64(guest)}
+	resCh := make(chan *pb.SearchResponse, len(c.connections))
+
 	for _, conn := range c.connections {
-		res, err := conn.Search(context.Background(), req)
-		if err != nil {
-			panic(err)
-		}
+		conn := conn
+		go func() {
+			res, err := conn.Search(context.Background(), req)
+			if err != nil {
+				panic(err)
+			}
+
+			resCh <- res
+		}()
+	}
+
+	for i := 0; i < len(c.connections); i++ {
+		res := <-resCh
 		smaller += int(res.Smaller)
 		same += int(res.Same)
 	}
+	close(resCh)
+
 	return smaller, same
 }
 
